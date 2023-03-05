60fc8aab3d8a89334344993a33b501e9fc49891a
Ola-la <112906056+Ola-la17@users.noreply.github.com>
2023-03-05 22:13:23 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,19 @@
+
+package seminar_05_calc;
+
+import sem_05_calc.controllers.CalcController;
+import sem_05_calc.models.Logger;
+import sem_05_calc.views.ViewCalc;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Logger logger = new Logger("GENERAL/main/java/seminar 05 calc/calclog.txt");
+        CalcController calc = new CalcController(logger);
+        ViewCalc view = new ViewCalc(calc);
+        logger.addToLog("Calc started");
+        view.run();
+        logger.addToLog("Calc stopped");
+    }
+}
